59a209d2ed4dbe8bfeb9b195c9f180f0dc253d02
Ben <iofelben+github@gmail.com>
2015-04-24 15:22:34 -0400

    google cloud messaging

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,34 +1,70 @@
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
+import com.jcabi.http.Request;
+import com.jcabi.http.Response;
+import com.jcabi.http.request.JdkRequest;
+import com.jcabi.http.wire.RetryWire;
 
+import java.io.*;
 import java.util.*;
 
 import static spark.Spark.*;
 
 public class Server {
-    static List<Buzz> buzzes = new ArrayList<>();
-    static Gson gson = new Gson();
     static class Buzz { String name, msg; }
 
-    public static void main(String... args) {
+    static List<String> bees = new ArrayList<>();
+    static List<Buzz> buzzes = new ArrayList<>();
+
+    static Gson gson = new Gson();
+
+    public static void main(String... args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(Server.class.getResourceAsStream("gcm.key")));
+        String gcmkey = br.readLine();
+        br.close();
+
         get("/buzzes", (req, res) -> {
             res.type("application/json");
             return buzzes;
         }, gson::toJson);
 
         put("/newBuzz", "application/json", (req, res) -> {
-            try {
-                Buzz b = gson.fromJson(req.body(), Buzz.class);
-                if (buzzes.stream().anyMatch(a -> a.msg.equals(b.msg))) // robot9000
-                    res.status(400);
-                else {
-                    buzzes.add(b);
-                    res.status(200);
-                }
-            } catch (JsonSyntaxException e) {
-                res.status(400);
-            }
+            Buzz newBuzz = gson.fromJson(req.body(), Buzz.class);
+            if (buzzes.stream().anyMatch(candidate -> candidate.msg.equals(newBuzz.msg))) halt(400);
+            buzzes.add(newBuzz);
+            pushNotif(newBuzz, gcmkey);
+            res.status(200);
             return "";
         });
+
+        exception(JsonSyntaxException.class, (e, req, res) -> res.status(400));
+    }
+
+    static void pushNotif(Buzz b, String gcmkey) {
+        class GCMMessage { // JSON data
+            List<String> registration_ids = bees;
+            Buzz data;
+            int time_to_live = 30; // seconds
+            //boolean delay_while_idle = true;
+        }
+
+        if (!bees.isEmpty())
+            new Thread(() -> {
+                GCMMessage gcm = new GCMMessage();
+                gcm.data = b;
+                try {
+                    Response res = new JdkRequest("https://android.googleapis.com/gcm/send")
+                            .through(RetryWire.class)
+                            .method(Request.POST)
+                            .header("Authorization", "key=" + gcmkey)
+                            .header("Content-Type", "application/json")
+                            .body().set(gson.toJson(gcm)).back()
+                            .fetch();
+                    assert (res.status() == 200) :
+                            String.format("GCM response was %d: %s\n%s\n", res.status(), res.reason(), res.body());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
     }
 }
